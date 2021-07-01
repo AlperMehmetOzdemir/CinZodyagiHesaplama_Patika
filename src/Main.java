b2dd6f8c124d13d192cd48fbaa61238ffe32b248
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2021-07-01 20:11:00 +0300

    Assignment completed.

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int doğumYılı;
+        String zodyak;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        doğumYılı = scanner.nextInt();
+
+        switch (doğumYılı % 12){
+            case 0:
+                zodyak = "Maymnun";
+                break;
+            case 1:
+                zodyak = "Horoz";
+                break;
+            case 2:
+                zodyak = "Köpek";
+                break;
+            case 3:
+                zodyak = "Domuz";
+                break;
+            case 4:
+                zodyak = "Fare";
+                break;
+            case 5:
+                zodyak = "Öküz";
+                break;
+            case 6:
+                zodyak = "Kaplan";
+                break;
+            case 7:
+                zodyak = "Tavşan";
+                break;
+            case 8:
+                zodyak = "Ejderha";
+                break;
+            case 9:
+                zodyak = "Yılan";
+                break;
+            case 10:
+                zodyak = "At";
+                break;
+            case 11:
+                zodyak = "Koyun";
+                break;
+            default:
+                zodyak = "Geçersiz";
+        }
+
+        System.out.println("Çin Zodyağı Burcunuz: " + zodyak);
+    }
+}
